111622613118316f3f9236742c318146d389110c
so1ji <mr.germ@mail.ru>
2019-10-01 18:15:32 +0300

    add swagger api-test

--- cypress/all-tests/api-tests/api-test-swagger.js
+++ cypress/all-tests/api-tests/api-test-swagger.js
@@ -4,15 +4,30 @@ describe('Test for swagger petstore', () => {
     })
 });
 
-it('Negative: Add pet', () => {
+it('Positive: Add pet', () => {
     cy.fixture('pet').then(pet => {
         cy.request({
             method: 'POST',
-            url: 'https://petstore.swagger.io/v2',
+            url: 'https://petstore.swagger.io/v2/pet',
             failOnStatusCode: false,
             body: {pet}
         }).then(response => {
-            expect(response.status).to.eq(405);
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property('status');
+        })
+    })
+});
+
+
+it('Positive: Update pet', () => {
+    cy.fixture('pet').then(pet => {
+        cy.request({
+            method: 'POST',
+            url: 'https://petstore.swagger.io/v2/pet',
+            failOnStatusCode: false,
+            body: {pet}
+        }).then(response => {
+            expect(response.status).to.eq(200);
         })
     })
 });
