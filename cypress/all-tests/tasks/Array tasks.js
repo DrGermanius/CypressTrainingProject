b118da10b3ff58fa2958f4d480293c89ebfee5ce
so1ji <mr.germ@mail.ru>
2019-10-25 12:10:17 +0300

    add a tasks

---
+++ cypress/all-tests/tasks/Array tasks.js
@@ -0,0 +1,12 @@
+cy.log("ARRAY TASKS")
+let planets = [
+    {planet: "Mercury", radius: 2440, density: 5.43, distance: 0.395},
+    {planet: "Venus", radius: 6052, density: 5.24, distance: 0.723},
+    {planet: "Earth", radius: 6378, density: 5.52, distance: 1},
+    {planet: "Mars", radius: 3396, density: 3.93, distance: 1.53},
+    {planet: "Jupiter", radius: 71492, density: 1.33, distance: 5.21},
+    {planet: "Saturn", radius: 60268, density: 0.69, distance: 9.551},
+    {planet: "Uranus", radius: 25559, density: 1.27, distance: 19.213},
+    {planet: "Neptune", radius: 24764, density: 1.64, distance: 30.07}
+]
+printPlanets(planets)
