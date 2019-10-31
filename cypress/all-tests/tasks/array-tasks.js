813eb02cd6102b410f8375df72d718f912dedaef
so1ji <mr.germ@mail.ru>
2019-10-31 18:11:10 +0300

    fix array sorting

--- cypress/all-tests/tasks/array-tasks.js
+++ cypress/all-tests/tasks/array-tasks.js
@@ -41,7 +41,15 @@ describe('Array Practice', () => {
         printPlanet(planets);
 
         cy.log("Sorting by name");
-        planets.sort((a, b) => a.name - b.name);
+        planets.sort((a, b) => {
+            if (a.name > b.name) {
+                return 1;
+            }
+            if (a.name < b.name) {
+                return -1;
+            }
+            return 0;
+        });
         printPlanet(planets);
 
 
