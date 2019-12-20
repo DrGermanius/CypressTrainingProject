6d3b60309bd2dc6a03f254c9c7d67a1c513a82f8
so1ji <mr.germ@mail.ru>
2019-12-20 14:56:35 +0300

    add Map and If-Else

--- cypress/all-tests/ui-tests/ui-test-oz.js
+++ cypress/all-tests/ui-tests/ui-test-oz.js
@@ -3,14 +3,44 @@ import Chance from "chance";
 
 describe('UI - test Practice', () => {
 
-    it('OZ.by test with count of goods more than 10', () => {
-        ozPage.changeCountOfGoods(chance.integer({min: 10, max: 100}));
+    it('Card: User is able to add single product', () => {
+        ozPage.open();
+        ozPage.putGoodsInBasket();
+        cy.get('.top-panel__userbar__cart__item').click();
+        cy.get('.goods-table-cell__line_title').should(($this) => {
+            expect($this).to.contain(`Как пасти котов`)
+        });
+    });
+
+
+    it('Card: User is able to change count of one product from single to multiple (more than 10)', () => {
+        let count = chance.integer({min: 10, max: 99});
+        ozPage.open();
+        ozPage.putGoodsInBasket();
+        cy.get('.top-panel__userbar__cart__item').click();
+        cy.get('.goods-table-cell__line_title').should(($this) => {
+            expect($this).to.contain(`Как пасти котов`)
+        });
+        ozPage.changeCountOfGoods(count);
+        cy.get('.deal-form-main__sub').should(($this) => {
+            expect($this).to.contain(`Итого ${count}`)
+        });
     })
 
 
 
-    it('OZ.by test with count of goods less than 10', () => {
-        ozPage.changeCountOfGoods(chance.integer({min: 1, max: 9}));
+    it('Card: User is able to change count of one product from single to multiple (less than 10)', () => {
+        let count = chance.integer({min: 1, max: 9});
+        ozPage.open();
+        ozPage.putGoodsInBasket();
+        cy.get('.top-panel__userbar__cart__item').click();
+        cy.get('.goods-table-cell__line_title').should(($this) => {
+            expect($this).to.contain(`Как пасти котов`)
+        });
+        ozPage.changeCountOfGoods(count);
+        cy.get('.deal-form-main__sub').should(($this) => {
+            expect($this).to.contain(`Итого ${count}`)
+        });
     }) ;
 
 
--- cypress/page-objects/ozPage.js
+++ cypress/page-objects/ozPage.js
@@ -1,32 +1,37 @@
 
 class OzPage {
 
+
+    static getData()
+    {
+
+    }
+
     open()
     {
-        cy.visit('https://oz.by/');
+        cy.fixture('ozby').then(data=>
+        {
+            cy.wrap(data).as('ozbyData')
+        });
+        cy.get('@ozbyData').then((ozbyData)=>
+        cy.visit(ozbyData.url)
+        );
     }
 
     putGoodsInBasket()
     {
-        cy.get("#top-s").click().type("Как пасти котов");
+        cy.fixture('ozby').then(data=>
+        {
+            cy.wrap(data).as('ozbyData')
+        });
+
+        cy.get('@ozbyData').then((ozbyData)=>
+            cy.get("#top-s").click().type(ozbyData.productName)
+        );
         cy.get('.top-panel__search__btn').click();
         cy.get('.addtocart-btn').click();
     }
 
-    checkItemInBasket()
-    {
-        cy.get('.top-panel__userbar__cart__item').click();
-        cy.get('.goods-table-cell__line_title').should(($this) => {
-            expect($this).to.contain(`Как пасти котов`)
-        });
-    }
-
-     checkCountOfItemsInBasket(count)
-    {
-        cy.get('.deal-form-main__sub').should(($this) => {
-            expect($this).to.contain(`Итого ${count}`)
-        });
-    }
 
     clearBasket() //didnt work
     {
@@ -36,24 +41,21 @@ class OzPage {
         cy.get('i-button_small').contains('Да, удалить').click(); //cant get
     }
 
+
     changeCountOfGoods(count)
     {
-        this.open();
-        this.putGoodsInBasket();
-        this.checkItemInBasket();
 
         cy.get('.i-amount-select').click();
 
         if(count < 10)
         {
             cy.get('.i-amount-select__value').contains(count).click();
-            this.checkCountOfItemsInBasket(count);
         }
         else
         {
             cy.get('.i-amount-select__value').contains('10+').click();
             cy.get('.i-input_value').type(count);
-            this.checkCountOfItemsInBasket(count);
+
         }
      //   this.clearBasket();
     }
